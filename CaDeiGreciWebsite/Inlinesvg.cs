de3bb26aceb82a69eec3e5dcbef178055c45041c
Luigi Trabacchin <trabacchin.luigi@gmail.com>
2022-04-13 22:19:53 +0200

    Initial commit

---
+++ CaDeiGreciWebsite/Inlinesvg.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaDeiGreciWebsite.TagHelpers
+{
+    public class Inlinesvg : TagHelper
+    {
+        private static String incipit = @"<svg xmlns=""http://www.w3.org/2000/svg""";
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        // PascalCase gets translated into kebab-case.
+        public String Src { get; set; }
+
+        public Inlinesvg(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+            //output.TagMode = TagMode.SelfClosing;
+            //output.Content.Clear();
+
+            var filePath = System.IO.Path.Combine(_webHostEnvironment.WebRootPath, Src);
+            using (var file = System.IO.File.OpenRead(filePath))
+            using (var sr = new StreamReader(file))
+            {
+                var readAmount = incipit.Length;
+                Span<Char> buffer = stackalloc Char[readAmount];
+                var fileStart = sr.Read(buffer);
+                if (!System.MemoryExtensions.Equals(buffer, incipit, StringComparison.OrdinalIgnoreCase))
+                    throw new ApplicationException("The provided path is not a valid svg");
+
+                output.Content.AppendHtml(buffer.ToString());
+                foreach (var attribute in context.AllAttributes)
+                {
+                    if (String.Equals(attribute.Name, "src", StringComparison.OrdinalIgnoreCase)) continue;
+                    output.Content.AppendHtml($@"{attribute.Name}=""{attribute.Value}""");
+                }
+                output.Content.AppendHtml(sr.ReadToEnd());
+            }
+        }
+    }
+}
---
+++ CaDeiGreciWebsite/wwwroot/js/ScrollOpacity.js
@@ -0,0 +1,31 @@
+﻿(function () {
+
+    /**
+     * @param {number} range
+     **/
+    function GetScrollPercentage(range) {
+        if (!isNaN(Number)) { throw "range should be a number"; }
+        let scrollTop = window.scrollY;
+        let percentage = 1 / range * scrollTop;
+        console.log(percentage);
+
+        percentage = percentage > 1 ? 1 : percentage < 0 ? 0 : percentage;
+        return percentage;
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        let firstSlide = document.getElementById('slide-first');
+        let navbarTop = document.getElementById('navbar-top');
+        let bg = navbarTop.getElementsByClassName('background')[0];
+        if (!(bg instanceof HTMLDivElement)) { throw "navbartop isn't a div"; }
+        let range = firstSlide.getBoundingClientRect().height - navbarTop.getBoundingClientRect().height;
+
+        window.addEventListener("resize", function (ev) {
+            range = firstSlide.getBoundingClientRect().height - navbarTop.getBoundingClientRect().height;
+            bg.style.opacity = GetScrollPercentage(range);
+        });
+        window.addEventListener('scroll', function () {
+            bg.style.opacity = GetScrollPercentage(range);
+        });
+    });
+})();
